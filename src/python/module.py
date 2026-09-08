5c4693575ec288e3d6007dccb47bd65f27401351
Guangtao Zhu <guangtaozhu@outlook.com>
2026-09-08 12:45:40 +1000

    cap-del: abstract filesystem (FAT)
    This commit allows users to initialise an FAT filesystem
    with attribute "optional=" a boolen value. The connection
    of an optional FAT to a client is delegated.
    
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- src/python/module.py
+++ src/python/module.py
@@ -322,7 +322,7 @@ libsdfgen.sdfgen_vmm_serialise_config.restype = c_bool
 libsdfgen.sdfgen_vmm_serialise_config.argtypes = [c_void_p, c_char_p]
 
 libsdfgen.sdfgen_lionsos_fs_fat.restype = c_void_p
-libsdfgen.sdfgen_lionsos_fs_fat.argtypes = [c_void_p, c_void_p, c_void_p, c_void_p, c_uint32]
+libsdfgen.sdfgen_lionsos_fs_fat.argtypes = [c_void_p, c_void_p, c_void_p, c_void_p, c_uint32, c_bool]
 libsdfgen.sdfgen_lionsos_fs_fat_connect.restype = c_bool
 libsdfgen.sdfgen_lionsos_fs_fat_connect.argtypes = [c_void_p]
 libsdfgen.sdfgen_lionsos_fs_fat_serialise_config.restype = c_bool
@@ -1510,6 +1510,7 @@ class LionsOs:
                 *,
                 blk: Sddf.Blk,
                 partition: int,
+                optional: bool = False,
             ):
                 if partition < 0:
                     raise Exception(
@@ -1517,7 +1518,9 @@ class LionsOs:
                     )
 
                 assert isinstance(blk, Sddf.Blk)
-                self._obj = libsdfgen.sdfgen_lionsos_fs_fat(sdf._obj, fs._obj, client._obj, blk._obj, partition)
+                self._obj = libsdfgen.sdfgen_lionsos_fs_fat(
+                    sdf._obj, fs._obj, client._obj, blk._obj, partition, optional
+                )
                 if self._obj is None:
                     raise Exception("failed to create FAT file system")
 
