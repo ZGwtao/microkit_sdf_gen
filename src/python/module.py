dbfc8f5dd539cd20ec797af24fe511596333114f
Guangtao Zhu <guangtaozhu@outlook.com>
2026-09-08 12:48:34 +1000

    blk: changeable client queue buffer capacity
    This commit allows the users to change the capacity of
    the block virtualiser buffer. When the number of clients
    connected to a block scales, the per-client buffer number
    will decrease.
    
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- src/python/module.py
+++ src/python/module.py
@@ -322,7 +322,7 @@ libsdfgen.sdfgen_vmm_serialise_config.restype = c_bool
 libsdfgen.sdfgen_vmm_serialise_config.argtypes = [c_void_p, c_char_p]
 
 libsdfgen.sdfgen_lionsos_fs_fat.restype = c_void_p
-libsdfgen.sdfgen_lionsos_fs_fat.argtypes = [c_void_p, c_void_p, c_void_p, c_void_p, c_uint32, c_bool]
+libsdfgen.sdfgen_lionsos_fs_fat.argtypes = [c_void_p, c_void_p, c_void_p, c_void_p, c_uint32, c_uint16, c_bool]
 libsdfgen.sdfgen_lionsos_fs_fat_connect.restype = c_bool
 libsdfgen.sdfgen_lionsos_fs_fat_connect.argtypes = [c_void_p]
 libsdfgen.sdfgen_lionsos_fs_fat_serialise_config.restype = c_bool
@@ -1510,6 +1510,7 @@ class LionsOs:
                 *,
                 blk: Sddf.Blk,
                 partition: int,
+                blk_queue_capacity: int = 128,
                 optional: bool = False,
             ):
                 if partition < 0:
@@ -1519,7 +1520,8 @@ class LionsOs:
 
                 assert isinstance(blk, Sddf.Blk)
                 self._obj = libsdfgen.sdfgen_lionsos_fs_fat(
-                    sdf._obj, fs._obj, client._obj, blk._obj, partition, optional
+                    sdf._obj, fs._obj, client._obj, blk._obj, partition,
+                    blk_queue_capacity, optional
                 )
                 if self._obj is None:
                     raise Exception("failed to create FAT file system")
